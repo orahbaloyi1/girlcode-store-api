e884c555b20f5f19576b525f655b58554baffdf1
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-06-27 19:00:56 +0200

    Connected API

--- Api.py
+++ Api.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)   #initiliaze flask application, name mean the module
 
-@app.route('/') # call route, decorator.
+@app.route('/')         # call route, decorator.
 def main():
     return"Girlcode store API"
